2c013142337a5ff18b01165fd070ca7a2635035f
MakangaChristopher <makangachristopher5@gmail.com>
2023-07-18 14:14:50 +0300

    Initial commit with models, widgets and one screen

---
+++ lib/models/hotel_models.dart
@@ -0,0 +1,28 @@
+class HostelModel {
+  final String hostelID;
+  final String name;
+  final String imageUrl;
+  final List<String> relatedImagesUrls;
+  final int price;
+  final String location;
+  final String description;
+  final List<String> amenities;
+  final String contactDetails;
+  final bool doubleRoomsAvailability;
+  final bool singleRoomsAvailability;
+
+  HostelModel(
+      {required this.hostelID,
+      required this.name,
+      required this.imageUrl,
+      required this.relatedImagesUrls,
+      required this.price,
+      required this.location,
+      required this.description,
+      required this.amenities,
+      required this.contactDetails,
+      required this.doubleRoomsAvailability,
+      required this.singleRoomsAvailability
+
+      });
+}
---
+++ lib/models/user_models.dart
@@ -0,0 +1,18 @@
+class UserModel {
+  final String userID;
+  final String name;
+  final String email;
+  final String userType;
+  final String profilePicture;
+  final int phoneNumber;
+
+  UserModel(
+      {required this.userID,
+      required this.name,
+      required this.email,
+      required this.userType,
+      required this.profilePicture,
+      required this.phoneNumber
+      
+      });
+}
