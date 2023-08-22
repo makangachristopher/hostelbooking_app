f86f1b2ddbee967987914a0c90ce679fc7317945
cedric_ahumuza <ahumuzacedric@gmail.com>
2023-08-22 08:11:00 +0000

    login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'signup.dart';
 import 'root_app.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -25,8 +26,14 @@ class _LoginScreenState extends State<LoginScreen> {
         password: password,
       );
 
-      Navigator.of(context)
-          .pushReplacement(MaterialPageRoute(builder: (_) => RootApp()));
+      // Store user's email and password in SharedPreferences
+      final prefs = await SharedPreferences.getInstance();
+      prefs.setString('userEmail', email);
+      prefs.setString('userPassword', password);
+
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (_) => RootApp()),
+      );
     } on FirebaseAuthException catch (e) {
       setState(() {
         _errorMessage = e.message ?? 'An unknown error occurred';
@@ -34,7 +41,11 @@ class _LoginScreenState extends State<LoginScreen> {
     }
   }
 
-  void _navigateToSignup() {
+  void _navigateToSignup() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove('userEmail');
+    prefs.remove('userPassword');
+
     Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => SignupScreen()),
@@ -43,6 +54,25 @@ class _LoginScreenState extends State<LoginScreen> {
 
   void _navigateToForgotPassword() {
     Navigator.pushNamed(context, '/signup');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSavedLoginDetails();
+  }
+
+  void _loadSavedLoginDetails() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedEmail = prefs.getString('userEmail');
+    final savedPassword = prefs.getString('userPassword');
+
+    if (savedEmail != null) {
+      _emailController.text = savedEmail;
+    }
+    if (savedPassword != null) {
+      _passwordController.text = savedPassword;
+    }
   }
 
   @override
